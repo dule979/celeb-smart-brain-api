45afc3548e8bdd2fb98d49cf817a390ad9e6af24
Dusan Ilic <dule979@gmail.com>
2022-04-30 01:16:28 +0200

    Added API endpoints

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'memorex',
+      database : 'smart-brain'
+    }
+  });
+
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('data flowing');
-});
+app.get('/', (req, res) => db.select('*').from('users').then(data => res.json(data)));
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
+app.put('/image', (req, res) => image.handleImage(req, res, db));
+app.post('/imageurl', (req, res) => image.handleImageApi(req, res));
 
-app.listen(3000, () => console.log('Server is running'));
-
-/*
-
-res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT = user
-
-*/
+app.listen(3001, () => console.log('Server is running'));
