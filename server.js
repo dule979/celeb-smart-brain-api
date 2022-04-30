584cd173d9c31fb6fba281f82565fb5e5a597bbd
Dusan Ilic <dule979@gmail.com>
2022-04-30 02:28:46 +0200

    Response updated

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.json('it is working'));
+app.get('/', (req, res) => res.send('it is working'));
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
